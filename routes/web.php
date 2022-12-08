a925cf246a15801cb49d0de3229120efef5ab87a
hanimauliza28 <maulizahani@gmail.com>
2022-12-08 09:13:41 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::get('logout', [AuthController::class, 'logout'])->name('auth.logout');
 Route::middleware(['ceklogin'])->group(function () {
     Route::get('/', [HomeController::class, 'index'])->name('index');
     Route::get('/dashboard', [HomeController::class, 'index'])->name('home.index');
+    Route::get('', [HomeController::class, 'index'])->name('home');
     Route::get('/query', [HomeController::class, 'query'])->name('home.query');
 });
 
